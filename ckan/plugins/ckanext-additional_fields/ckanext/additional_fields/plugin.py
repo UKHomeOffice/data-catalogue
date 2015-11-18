29d028dc652535af637c671f88d4d76d265d3949
Michael Hughes <mikee.hughes@gmail.com>
2015-11-18 11:34:21 +0000

    Put business areas into vocab_ so that we can facet on it

--- ckan/plugins/ckanext-additional_fields/ckanext/additional_fields/plugin.py
+++ ckan/plugins/ckanext-additional_fields/ckanext/additional_fields/plugin.py
@@ -1,8 +1,48 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import json
 
 class Additional_FieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
+
+
+    #IFacet interface
+
+    def dataset_facets(self, facets_dict, package_type):
+        facets_dict['vocab_business_area'] = toolkit._('Business areas')
+        facets_dict['security_classification'] = toolkit._('Security Classification')
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        facets_dict['vocab_business_area'] = toolkit._('Business areas')
+        facets_dict['security_classification'] = toolkit._('Security Classification')
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        facets_dict['vocab_business_area'] = toolkit._('Business areas')
+        facets_dict['security_classification'] = toolkit._('Security Classification')
+        return facets_dict
+
+    def before_index(self, pkg_dict):
+        if 'extras_business_area' in pkg_dict:
+            ba = self.parse_multi_value(pkg_dict['extras_business_area'])
+            pkg_dict['vocab_business_area'] = ba
+        return pkg_dict
+
+    def parse_multi_value(self, value):
+        try:
+            return json.loads(value)
+        except ValueError:
+            return [value]
+
+
+    def is_fallback(self):
+        return True
+
+    def package_types(self):
+        return []
