d8a18543aeb011390124d02fd3790474fa7c4f34
Michael Hughes <mikee.hughes@gmail.com>
2015-11-18 16:50:42 +0000

    Load schema and switch values for labels when sending to solr

--- ckan/plugins/ckanext-additional_fields/ckanext/additional_fields/plugin.py
+++ ckan/plugins/ckanext-additional_fields/ckanext/additional_fields/plugin.py
@@ -1,6 +1,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import json
+import os
 
 class Additional_FieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
@@ -29,10 +30,17 @@ class Additional_FieldsPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetFor
         return facets_dict
 
     def before_index(self, pkg_dict):
-        if 'extras_business_area' in pkg_dict:
-            ba = self.parse_multi_value(pkg_dict['extras_business_area'])
-            pkg_dict['vocab_business_area'] = ba
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'schema.json')) as data_file:
+            data = json.load(data_file)
+            for dataset in data['dataset_fields']:
+                if dataset['field_name'] == 'business_area' and 'extras_business_area' in pkg_dict:
+                    ba = self.parse_multi_value(pkg_dict['extras_business_area'])
+                    pkg_dict['vocab_business_area'] = [self.map_to_label(dataset, value) for value in ba]
         return pkg_dict
+
+    def map_to_label(self, definition, value):
+        matching = [d['label'] for d in definition['choices'] if d['value'] == value]
+        return (matching or [value])[0]
 
     def parse_multi_value(self, value):
         try:
