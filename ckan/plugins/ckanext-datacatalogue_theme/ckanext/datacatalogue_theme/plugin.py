b67c5e85cdd41a4ed69d55666918c316b15c7407
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-03-22 10:20:46 +0000

    Added middleware to prevent non auth users seeing anything

--- ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/plugin.py
+++ ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/plugin.py
@@ -1,13 +1,35 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+non_auth_list = ['/user/login', '/user/register', '/user/reset']
 
 class Datacatalogue_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IMiddleware)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'datacatalogue_theme')
+
+
+    # IMiddleware
+    def make_middleware(self, app, config):
+        app = Middleware(app)
+        return app
+
+class Middleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+	if environ.get('REMOTE_USER') is None:
+	    if environ.get('PATH_INFO') in non_auth_list:
+	        return self.app(environ, start_response)
+	    else:
+	        start_response('401', [('Location', 'user/login')])
+	        return []
+	else:
+	    return self.app(environ, start_response)
+        s
