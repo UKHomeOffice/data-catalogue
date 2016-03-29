ff2b940ee4e2727ad3fe837b902b4b229aeab212
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-03-29 13:24:41 +0100

    first test

--- ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/tests/test_plugin.py
+++ ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/tests/test_plugin.py
@@ -1,5 +1,7 @@
 """Tests for plugin.py."""
 import ckanext.datacatalogue_theme.plugin as plugin
+from homeoffice.datacatalogue.auth_middleware import DCAuthMiddleware
 
 def test_plugin():
+    
     pass
