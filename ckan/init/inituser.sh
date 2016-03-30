933450f40ce4f57ce32876f88991f5d699a42991
Simon Greensmith <simon.greensmith@digital.homeoffice.gsi.gov.uk>
2016-03-30 11:31:09 +0100

    CDC-81 database initialisation for ytp-comments plugin

--- ckan/init/inituser.sh
+++ ckan/init/inituser.sh
@@ -3,3 +3,7 @@
 echo Creating admin user
 "$CKAN_HOME"/bin/paster --plugin=ckan user add admin email=admin@example.com password=admin --config=/etc/ckan/default/ckan.ini ;
 "$CKAN_HOME"/bin/paster --plugin=ckan sysadmin add admin --config=/etc/ckan/default/ckan.ini ;
+
+# TODO move this into a `initcomments.sh` script
+echo Configuring DB \for comments
+"$CKAN_HOME"/bin/paster --plugin=ckanext-ytp-comments initdb --config=/etc/ckan/default/ckan.ini ;
