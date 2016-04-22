ab00682e24fc54ffdceb5465247568025cf91476
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-22 09:50:51 +0100

    build container

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#run this script from the root of the repo.
 #get access to push to repo.
-version=$(cat ../versions/ckan_container_version)
-docker build -t quay.io/homeofficedigital/data-catalogue:$version.$BUILD_NUMBER ../
 
-docker push quay.io/homeofficedigital/data-catalogue:$version.$BUILD_NUMBER
+version=$(grep -o '.*build' versions/ckan_container_version)
+docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER ../
+
+docker push quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER
+
+echo $version-$BUILD_NUMBER > ../versions/ckan_container_version
