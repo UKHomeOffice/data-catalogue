e31d29f5b3e11b9eb4e20ca10f5719a0279bf420
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-22 10:16:20 +0100

    changed path for build

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 echo "run this script in jenkins only."
 version=$(grep -o '.*build' versions/ckan_container_version)
 echo "building version number $version-$BUILD_NUMBER"
-docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER ../
+docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
 
 echo "Pushing version number $version-$BUILD_NUMBER"
 docker push quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER
