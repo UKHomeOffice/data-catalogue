d79170bf0f7f156e154b417b95f1fe2d25627f4f
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-22 09:59:34 +0100

    added permissions and output

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-#run this script from the root of the repo.
+#!/bin/bash.
 #get access to push to repo.
-
+echo "run this script in jenkins only."
 version=$(grep -o '.*build' versions/ckan_container_version)
+echo "building version number $version-$BUILD_NUMBER"
 docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER ../
 
+echo "Pushing version number $version-$BUILD_NUMBER"
 docker push quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER
 
 echo $version-$BUILD_NUMBER > ../versions/ckan_container_version
