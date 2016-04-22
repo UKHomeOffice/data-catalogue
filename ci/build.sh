27aa84a2f4cf195f1832c1e86663aa70ecce0dd2
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-22 15:59:24 +0100

    fixed bug in decryption

--- ci/build.sh
+++ ci/build.sh
@@ -4,12 +4,12 @@ echo "run this script in jenkins only."
 source ci/s3.cfg
 version=$(grep -o '.*build' versions/ckan_container_version)
 echo "building version number $version-$BUILD_NUMBER"
-docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
+docker build -t quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
 
 #get docker config for pushing to s3
-s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/config.json.encrypted
+s3secrets --region ${AWS_DEFAULT_REGION} -p dsp-ci s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/config.json.encrypted
 
 echo "Pushing version number $version-$BUILD_NUMBER"
-docker push quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER
+docker push quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER
 
-echo $version-$BUILD_NUMBER > ../versions/ckan_container_version
+echo $version-$BUILD_NUMBER > versions/ckan_container_version
