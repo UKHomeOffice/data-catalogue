078bfd1b0bea4c90d3761e97be38f74b178b58db
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-22 15:36:22 +0100

    get secrets from s3

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #get access to push to repo.
 echo "run this script in jenkins only."
+source ci/s3.cfg
 version=$(grep -o '.*build' versions/ckan_container_version)
 echo "building version number $version-$BUILD_NUMBER"
 docker build -t quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
+
+#get docker config for pushing to s3
+s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/config.json.encrypted
 
 echo "Pushing version number $version-$BUILD_NUMBER"
 docker push quay.io/homeofficedigital/data-catalogue:$version-$BUILD_NUMBER
