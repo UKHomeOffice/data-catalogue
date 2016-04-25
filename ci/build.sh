a1d21a0c354e63dd6dc1baa87fd726d746a9c92e
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-25 15:33:20 +0100

    bugfix

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 #get access to push to repo.
 echo "run this script in jenkins only."
 source ci/s3.cfg
-version=$(grep versions/ckan_container_version)
+version=$(cat versions/ckan_container_version)
 echo "building version number $version-$BUILD_NUMBER"
 docker build -t quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
 
@@ -10,6 +10,6 @@ docker build -t quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBE
 s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/docker/config.json.encrypted
 
 echo "Pushing version number $version-$BUILD_NUMBER"
-docker push quay.io/ukhomeofficedigital/data-catalogue:$version-build-$BUILD_NUMBER
+docker push quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER
 
 echo $version-$BUILD_NUMBER > versions/ckan_container_version
