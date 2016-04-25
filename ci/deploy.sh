58139ba228b463bd8352b6e7be155951b455b6a5
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-25 17:23:47 +0100

    added env variable

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,7 @@
 # Script used as deployment entrypoint
 set -e
 source ci/s3.cfg
+env=dev
 #get kubeconfig from s3 bucket
 s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.kube shared/kube/config.encrypted
 
@@ -9,4 +10,4 @@ docker run --rm \
  -v /home/giles/.kube:/root/.kube/ \
  -v ${PWD}:/var/lib/deploy \
  quay.io/ukhomeofficedigital/kb8or:v0.6.12 -e $env \
- --log-level debug data-catalogue.yml
+ data-catalogue.yml
