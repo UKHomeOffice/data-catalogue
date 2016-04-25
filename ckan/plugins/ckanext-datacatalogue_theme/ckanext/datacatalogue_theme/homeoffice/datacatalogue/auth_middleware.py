23c77052a795b8d0f936c78419cb1c0b76b1821c
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-04-25 12:39:40 +0100

    Created some tests for checking headers and got api working again

--- ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/homeoffice/datacatalogue/auth_middleware.py
+++ ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/homeoffice/datacatalogue/auth_middleware.py
@@ -1,4 +1,7 @@
 # /ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/plugin.py
+import logging
+
+log = logging.getLogger(__name__)
 
 non_auth_list = ['/user/login', '/user/register', '/user/reset']
 
@@ -6,11 +9,18 @@ class DCAuthMiddleware(object):
     def __init__(self, app):
         self.app = app
     def __call__(self, environ, start_response):
-        if environ.get('REMOTE_USER') is None:
-            if environ.get('PATH_INFO') in non_auth_list:
-                return self.app(environ, start_response)
-            else:
-                start_response('401', [('Location', 'user/login')])
-                return []
+        if self.checkUser(environ):
+            return self.app(environ, start_response)
         else:
-            return self.app(environ, start_response) 
+            start_response('401', [('Location', 'user/login')])
+            return []
+    #Check if there is a user or an api key in the headers, or if it is one of the 
+    #non-auth pages
+    def checkUser(self, environ):
+        if environ.get('REMOTE_USER') is None and environ.get('HTTP_X_CKAN_API_KEY') is None:
+            if environ.get('PATH_INFO') in non_auth_list:
+                return True
+            else:
+                return False
+        else:
+            return True
