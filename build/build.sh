3fe45500b986948904bad470a336acb38e20738a
Giles Hinchcliff <gileshinchcliff@yahoo.com>
2016-04-26 16:19:17 +0100

    bugfix

--- build/build.sh
+++ build/build.sh
@@ -7,7 +7,7 @@ echo "building version number $version-$BUILD_NUMBER"
 docker build -t quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER .
 
 #get docker config for pushing to s3
-s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/docker/config.json.encrypted
+/opt/bin/s3secrets --region ${AWS_DEFAULT_REGION} s3 get --bucket ${SECRETS_BUCKET} -d ~/.docker shared/docker/config.json.encrypted
 
 echo "Pushing version number $version-$BUILD_NUMBER"
 docker push quay.io/ukhomeofficedigital/data-catalogue:$version-$BUILD_NUMBER
