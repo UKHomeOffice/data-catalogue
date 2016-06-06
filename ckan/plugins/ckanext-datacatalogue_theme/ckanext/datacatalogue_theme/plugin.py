98a3a5d351211654e4c3d1ad04837145c5ec4b65
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-06-06 10:35:48 +0100

    added more debug for solr

--- ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/plugin.py
+++ ckan/plugins/ckanext-datacatalogue_theme/ckanext/datacatalogue_theme/plugin.py
@@ -131,6 +131,7 @@ class Datacatalogue_DBPlugin(plugins.SingletonPlugin):
         config['solr_password'] = solr_password
         print("Talking to solr on " + config['solr_url'])
         print("with the user " + config['solr_user'])
+        print("and password " + config['solr_password'])
 
     def readCreds(self, creds):
         if(creds is None or ":" not in creds or creds == ":"):
