13df27ce706cac989b5991b7c19c5c2c424a3f1f
christopher.davidson@btinternet.com <christopher.davidson@btinternet.com>
2016-08-10 14:26:14 +0100

    printing database details

--- ckan/ckan/environment.py
+++ ckan/ckan/environment.py
@@ -278,6 +278,8 @@ def update_config():
             ' removed in a future release. Use CKAN_SQLALCHEMY_URL instead.'
         log.warn(msg)
         config['sqlalchemy.url'] = ckan_db
+    print("DATABASE_HOST" + os.environ.get("DATABASE_HOST", 'No Database host'))
+    print("DATABASE_USER" + os.environ.get("DATABASE_USER", 'No Database user'))
 
     for option in CONFIG_FROM_ENV_VARS:
         from_env = os.environ.get(CONFIG_FROM_ENV_VARS[option], None)
